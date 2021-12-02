49e133b934055f574597b8ed1cc1fdca62ee12da
WiklundViktor <viktor.wiklund@weknowit.nu>
2021-12-02 15:38:27 +0100

    s

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
@@ -8,9 +9,43 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+def calculateNrOfIncrement(dataArray: np.ndarray):
+    difference = np.diff(dataArray)
+    return np.sum(np.array(difference) > 0, axis=0)
 
+def flatList(dataArray: np.ndarray):
+    sum = np.zeros(len(dataArray)-2)
+    for i in range(len(dataArray)-2):
+        sum[i] = np.sum(dataArray[i:i+3])
+    return sum
+
+def commandSubmarine(command):
+    print(command)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    test = np.loadtxt("input.txt")
+    #inputCommand = np.loadtxt("inputCommand.txt")
+    sum = flatList(test)
+    #commandSubmarine(inputCommand)
+
+    lines = []
+    with open('inputCommand.txt') as f:
+        lines = f.readlines()
+
+    countFor = 0
+    countUp = 0
+    aim = 0
+    for line in lines:
+        x = line.split(" ")
+        if x[0] == "forward":
+            countFor = countFor + int(x[1])
+        if x[0] == "up":
+            countUp = countUp +  int(x[1])
+        if x[0] == "down":
+            print(x)
+            countUp = countUp -int(x[1])
+    print(countUp*countFor)
+        #print(f'line {count}: {line}')
+        #print(calculateNrOfIncrement(sum))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
