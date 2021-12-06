176fa58e99922ac205f29763d549755ff6b5dad1
ViktorChalmers <vikwik@student.chalmers.se>
2021-12-06 13:13:30 +0100

    d

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ def opentxt(string):
 
 
 def playBingo(lines):
-    randomDraws = lines[0]
+    randomDraws = lines[0].split(",")
+    print(randomDraws)
     boardLines = lines[2:]
-    nrOfBoards = int((len(boardLines) - 1) / 5)
-
+    nrOfBoards = int((len(boardLines)+1) / 6)
+    print(boardLines)
     boards = [[[
         {
-            "bingo": False,
+            "marked": False,
             "number": int(
-                boardLines[j + k * 6].replace(" ", ",").replace(",,", ",").replace(",", " ").strip().split(" ")[i])
+                            boardLines[j + k * 6].replace(" ", ",").replace(",,", ",").replace(",", " ").strip().split(" ")[i]
+                         )
         }
         for i in range(5)
     ]
@@ -27,16 +29,43 @@ def playBingo(lines):
         for k in range(nrOfBoards)
     ]
     #TODO continue program for all randomDraws and check for winner
-    for board in boards:
-        for row in board:
-            for number in row:
-                if number["number"] == int(randomDraws[0]):
-                    number["bingo"] = True
-    print(randomDraws[0])
-    print(boards[2][4][4])
+    i=0
+    while(checkWinner(boards) and i < 13):
+        for board in boards:
+            for row in board:
+                for number in row:
+                    if number["number"] == int(randomDraws[i]):
+                        number["marked"] = True
+        #print(randomDraws[i])
+        finalBoard  = board
+        finalNumber = int(randomDraws[i])
+        print(i, randomDraws[i])
+        i+=1
+    print(finalBoard)
+    score = 0
+    for row in finalBoard:
+        for num in row:
+            if not num["marked"]:
+                score += num["number"]
+    print(score*finalNumber)
+    #print(boards[2][0])
 
+def checkWinner(boards):
+    for board in boards:
+        for i in range(len(board)):
+            bingoRow = 0
+            bingoCol = 0
+            for j in range(len(board)):
+                if board[i][j]["marked"]:
+                    bingoRow+=1
+                if board[j][i]["marked"]:
+                    bingoCol+=1
+                if bingoRow>4 or bingoCol > 4:
+                    #print(board)
+                    return False
+    return True
 if __name__ == '__main__':
-    lines = opentxt("bingoInputTest.txt")
+    lines = opentxt("bingoInput.txt")
     playBingo(lines)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ trounleshoot.py
@@ -0,0 +1,25 @@
+lines = []
+with open("bingoInput.txt") as f:
+    lines = f.readlines()
+
+
+randomDraws = lines[0].split(",")
+#print(randomDraws)
+boardLines = lines[2:]
+nrOfBoards = int((len(boardLines)+1) / 6)
+print(nrOfBoards)
+#print(boardLines)
+boards = [[[
+    {
+        "marked": False,
+        "number": int(
+                        boardLines[j + k * 6].replace(" ", ",").replace(",,", ",").replace(",", " ").strip().split(" ")[i]
+                     )
+    }
+    for i in range(5)
+]
+    for j in range(5)
+]
+    for k in range(nrOfBoards)
+]
+print(boards)
