954d0c890c4c654b442d543ccb21130b18850ee0
vwiklund <v.wiklund@hotmail.com>
2021-12-07 08:23:48 +0100

    s

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 if __name__ == '__main__':
-    with open("input.txt") as f:
+    with open("inputTest.txt") as f:
         lines = f.readlines()
     position = []
     for line in lines[0].split(","):
         position.append(int(line))
 
-    print(position)
-    #position = [16,1,2,0,4,2,7,1,2,14]
-    print(np.median(position))
     count = 0
-    counta = 0
+    target = int(np.median(position))
+    steps = np.array(position) - target
+    count += sum(abs(steps))
+    print(count)
 
-    #print(sum(1,5))
-    length = max(position)
-    print(max(position))
-    tar = np.zeros(length)
-    for target in range(length):
-        #target = round(np.mean(position))
-        steps = np.array(position) - target
-        count = 0
-        counta = 0
-        #print(steps)
-        #print(round(np.mean(position)))
-        for step in steps:
-            counta += abs(step)
-            for i in range(abs(step)+1):
-                count += i
-        tar[target] = count
-        print(target,count)
-    print(min(tar))
+    count = 0
+    target = (round(np.mean(position)))
+    steps = np.array(position) - target
+    count = 0
+    print(target)
+    for step in steps:
+        for i in range(abs(step)+1):
+            count += i
+    print(count)
